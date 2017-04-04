07e835cd430e838f90837f642b72e4a0485252f7
seedub <carltonsaddress@hotmail.com>
2017-04-03 17:03:28 -0700

    added common_pkg body

---
+++ common_pkg.sql
@@ -0,0 +1,11 @@
+create or replace package body common_pkg as
+  function func1 (p_param varchar2) is
+    l_chr varchar2(10);
+   
+    begin
+
+      l_chr := 'x';
+
+    end func1;
+
+end common_pkg;
