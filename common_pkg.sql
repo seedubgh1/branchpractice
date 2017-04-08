f8b96d2f11afb6444e6cf5656ff35b88a9bd5ea0
seedub <carltonsaddress@hotmail.com>
2017-04-07 17:11:50 -0700

    initial dev branch commit; added function dev_only

--- common_pkg.sql
+++ common_pkg.sql
@@ -8,6 +8,11 @@ create or replace package body common_pkg as
 
     end func1;
 ------------
+  function dev_only (p_param varchar2) is
+    begin
+      null;
+    end dev_only;
+------------
   function func2 (p_param varchar2) is
     l_chr varchar2(10);
    
